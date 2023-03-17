088906e393d90f5b69a6e84de8b2e1149029fee0
Robi <robi.maliqi@hotmail.co.uk>
2023-03-17 13:55:25 +0000

    added search feature

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,12 +1,23 @@
-require 'json'
-require 'open-uri'
-
 class NewsController < ApplicationController
+  require 'open-uri'
+  require 'json'
 
   def index
-    url = "https://gnews.io/api/v4/search?q=example&lang=en&country=gb&max=10&apikey=#{ENV['API_KEY']}"
-
-    news_serialized = URI.open(url).read
-    @articles = JSON.parse(news_serialized)["articles"]
+    @articles = fetch_articles('example')
   end
+
+  def search
+    search_query = params[:q]
+    @articles = fetch_articles(search_query)
+    render :index
+  end
+
+  private
+
+  def fetch_articles(query)
+    url = "https://gnews.io/api/v4/search?q=#{URI.encode_www_form_component(query)}&lang=en&country=gb&max=10&apikey=#{ENV['API_KEY']}"
+    news_serialized = URI.open(url).read
+    JSON.parse(news_serialized)["articles"]
+  end  
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'news#index'
+  get '/search', to: 'news#search', as: 'search_news'
 end
