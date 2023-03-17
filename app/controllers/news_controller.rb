7d4934c055b47f945fff678bf5a277b9c293b47e
Robi <robi.maliqi@hotmail.co.uk>
2023-03-17 11:54:34 +0000

    added code to controller to access data in json

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,12 @@
+require 'json'
+require 'open-uri'
+
 class NewsController < ApplicationController
+
   def index
+    url = "https://gnews.io/api/v4/search?q=example&lang=en&country=gb&max=10&apikey=#{ENV['API_KEY']}"
+
+    news_serialized = URI.open(url).read
+    @articles = JSON.parse(news_serialized)["articles"]
   end
 end
