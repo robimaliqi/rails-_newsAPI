ef196b0155ce97b08a5b41333a167a7bf0a78304
Robi <robi.maliqi@hotmail.co.uk>
2023-03-19 22:31:39 +0000

    WIP: if user inputs empty search

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -8,33 +8,35 @@ class NewsController < ApplicationController
   API_KEY = ENV['API_KEY']
   URL     = "https://gnews.io/api/v4/search?q=%<query>s&lang=en&country=%<country>s&max=10&apikey=#{API_KEY}"
 
-  public
-  def initialize
-    @articles = []
-  end
+  # public
 
   def index
-    @articles = fetch_articles('example')
+    @articles = fetch_articles
   end
 
   def search
-    search_query = params[:q]
+    if params[:q].empty?
+      search_query = 'news'
+    else
+      search_query = params[:q]
+    end
+
     @articles = fetch_articles(search_query)
     render :index
   end
 
-  private
+  # private
 
   def fetch_articles(query)
     user_input = URI.encode_www_form_component(query)
-    url_search = URL % {query: user_input, country: @country_code}
+    url_search = URL % {query: user_input, country: set_location}
     news_serialized = URI.open(url_search).read
     JSON.parse(news_serialized)["articles"]
   end  
 
   def set_location
     location = request.location
-    @country_code = location.country_code
+    location.country_code
   end
 end
 
