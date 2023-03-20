f28503300f34b4ff8f4c1c5dd021e8c66086192a
Robi <robi.maliqi@hotmail.co.uk>
2023-03-20 11:27:56 +0000

    refactored controller more

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -8,35 +8,19 @@ class NewsController < ApplicationController
   API_KEY = ENV['API_KEY']
   URL     = "https://gnews.io/api/v4/search?q=%<query>s&lang=en&country=%<country>s&max=10&apikey=#{API_KEY}"
 
-  # public
-
   def index
-    @articles = fetch_articles
+    @articles = fetch_articles('news')
   end
 
   def search
-    if params[:q].empty?
-      @articles = fetch_articles
-    else
-      search_query = params[:q]
-      @articles = fetch_articles(search_query)
-    end
-
+    search_query = params[:q]
+    @articles = fetch_articles(search_query)
     render :index
   end
 
-  # private
-
-  def fetch_articles(query = 'news')
+  def fetch_articles(query)
     user_input = URI.encode_www_form_component(query)
-    url_search = URL % {query: user_input, country: set_location}
-    news_serialized = URI.open(url_search).read
-    JSON.parse(news_serialized)["articles"]
-  end  
-
-  def set_location
-    location = request.location
-    location.country_code
+    url_search = format(URL, query: user_input, country: request.location.country_code)
+    JSON.parse(URI.open(url_search).read)['articles']
   end
-end
 
